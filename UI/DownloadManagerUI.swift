8c26ebd0340aacf63a8921aa5d6530d3043b7eab
chen <2153953+enefry@users.noreply.github.com>
2025-06-27 22:30:16 +0800

    修改UI，还差全局速度显示+时间预计

--- UI/DownloadManagerUI.swift
+++ UI/DownloadManagerUI.swift
@@ -147,60 +147,88 @@ private struct GlobalActionsView: View {
     @State var action = ActionBlock()
 
     var body: some View {
-        HStack {
-            Text("下载中: \(viewModel.counter.actives)")
-                .font(.subheadline)
-                .foregroundColor(.secondary)
-            Spacer()
+        VStack {
             HStack {
-                Button(action: {
-                    action = ActionBlock(show: true, type: .pause, title: "暂停所有", message: "暂停所有任务？", action: viewModel.pauseAll)
-                }, label: {
-                    Image(systemName: "pause.circle")
-                        .font(.system(size: 32))
-                        .frame(width: 40, height: 40)
-                })
-                .disabled(viewModel.counter.actives == 0)
+                Text("任务: \(viewModel.counter.actives)/\(viewModel.counter.all)")
+                    .font(.headline)
+                    .foregroundColor(.secondary)
+                    .frame(maxWidth: .infinity, alignment: .leading)
+                Text("速度: \(viewModel.counter.actives)/\(viewModel.counter.all)")
+                    .font(.headline)
+                    .foregroundColor(.secondary)
+                    .frame(maxWidth: .infinity, alignment: .leading)
+            }
+            HStack {
+                Text("时间: \(viewModel.counter.actives)/\(viewModel.counter.all)")
+                    .font(.headline)
+                    .foregroundColor(.secondary)
+                    .frame(maxWidth: .infinity, alignment: .leading)
+                Text("进度: \(viewModel.counter.actives)/\(viewModel.counter.all)")
+                    .font(.headline)
+                    .foregroundColor(.secondary)
+                    .frame(maxWidth: .infinity, alignment: .leading)
+            }
+            HStack {
+                Button(action: viewModel.pauseAll
+//                        {action = ActionBlock(show: true, type: .pause, title: "暂停所有", message: "暂停所有任务？", action: viewModel.pauseAll)}
+                    , label: {
+                        Image(systemName: "pause.circle")
+                            .font(.system(size: 28))
+                            .frame(maxWidth: .infinity)
+                            .frame(height: 32)
+                    })
+                    .buttonStyle(.borderedProminent)
+                    .disabled(viewModel.counter.actives == 0)
 
-                Button(action: {
-                    action = ActionBlock(show: true, type: .resume, title: "恢复所有", message: "恢复所有下载？", action: viewModel.resumeAll)
-                }, label: {
-                    Image(systemName: "play.circle")
-                        .font(.system(size: 32))
-                        .frame(width: 40, height: 40)
-                })
-                .disabled(viewModel.counter.pause == 0)
+                Button(action: viewModel.resumeAll
+//                        {action = ActionBlock(show: true, type: .resume, title: "恢复所有", message: "恢复所有下载？", action: )}
+                    , label: {
+                        Image(systemName: "play.circle")
+                            .font(.system(size: 28))
+                            .frame(maxWidth: .infinity)
+                            .frame(height: 32)
+                    })
+                    .buttonStyle(.borderedProminent)
+                    .disabled(viewModel.counter.pause == 0)
 
                 Button(action: {
                     action = ActionBlock(show: true, type: .cancel, title: "停止所有", message: "停止所有下载？", action: viewModel.manager.cancelAll)
                 }, label: {
                     Image(systemName: "stop.circle")
-                        .font(.system(size: 32))
-                        .frame(width: 40, height: 40)
+                        .font(.system(size: 28))
+                        .frame(maxWidth: .infinity)
+                        .frame(height: 32)
                 })
+                .buttonStyle(.borderedProminent)
                 .disabled(viewModel.counter.actives == 0)
 
-                Button(action: {
-                    action = ActionBlock(show: true, type: .cancel, title: "移除完成", destructive: true, message: "移除所有已经完成的任务？", action: viewModel.manager.removeAllFinished)
-                }, label: {
-                    Image(systemName: "clear.fill")
-                        .font(.system(size: 32))
-                        .frame(width: 40, height: 40)
-                        .foregroundColor(.red)
-                })
-                .disabled(viewModel.counter.finished == 0)
+                Button(action: viewModel.manager.removeAllFinished
+//                       {action = ActionBlock(show: true, type: .cancel, title: "移除完成", destructive: true, message: "移除所有已经完成的任务？", action: viewModel.manager.removeAllFinished)}
+                    , label: {
+                        Image(systemName: "clear.fill")
+                            .font(.system(size: 28))
+                            .frame(maxWidth: .infinity)
+                            .frame(height: 32)
+                            .foregroundColor(.red)
+                    })
+                    .buttonStyle(.borderedProminent)
+                    .disabled(viewModel.counter.finished == 0)
 
                 Button(action: {
                     action = ActionBlock(show: true, type: .remove, title: "删除所有", message: "删除所有下载任务（包括未完成的的临时文件）？", action: viewModel.removeAll)
                 }, label: {
                     Image(systemName: "trash.circle.fill")
-                        .font(.system(size: 32))
-                        .frame(width: 40, height: 40)
+                        .font(.system(size: 28))
+                        .frame(maxWidth: .infinity)
+                        .frame(height: 32)
                         .foregroundColor(.red)
                 })
+                .buttonStyle(.borderedProminent)
                 .disabled(viewModel.counter.all == 0)
-            }.opacity(viewModel.counter.all > 0 ? 1 : 0)
+            }
+//            .opacity(viewModel.counter.all > 0 ? 1 : 0)
         }
+        .padding()
         .alert(action.title, isPresented: $action.show, actions: {
             Button(
                 role: .cancel,
@@ -238,7 +266,7 @@ private struct FilterButtonsView: View {
     @Binding var selectedFilter: DownloadStateFilter
 
     var body: some View {
-        Picker("Filter", selection: $selectedFilter) {
+        Picker("", selection: $selectedFilter) {
             ForEach(DownloadStateFilter.cases, id: \.self) { filter in
                 Text(filter.rawValue).tag(filter)
             }
