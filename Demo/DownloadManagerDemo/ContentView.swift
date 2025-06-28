10a180746bca5ac8d83210ceb54705bcaf1619c0
chen <2153953+enefry@users.noreply.github.com>
2025-06-28 19:52:15 +0800

    增加ua支持

--- Demo/DownloadManagerDemo/ContentView.swift
+++ Demo/DownloadManagerDemo/ContentView.swift
@@ -13,22 +13,31 @@ import SwiftUI
 struct ContentView: View {
     @State var manager: DownloadManagerProtocol = CreateDownloadManager()
     @State var text: String = ""
+    @State var userAgent: String = ""
     var body: some View {
         VStack {
             HStack {
                 TextField("连接", text: $text)
+                TextField("user-agent", text: $userAgent)
                 Button("下载") {
                     if let url = URL(string: text),
                        let name = (text as? NSString)?.lastPathComponent {
                         let dest = FileManager.default.temporaryDirectory.appending(component: name)
                         Task {
-                            await manager.download(url: url, destination: dest, configuration: nil)
+                            var config = DownloadTaskConfiguration()
+                            if userAgent.count > 0 {
+                                config.headers["user-agent"] = userAgent
+                                UserDefaults.standard.set(userAgent, forKey: "user-agent")
+                            }
+                            await manager.download(url: url, destination: dest, configuration: config)
                         }
                     }
                 }
             }
             DownloadManagerView(manager: manager)
-        }
+        }.onAppear(perform: {
+            userAgent = UserDefaults.standard.string(forKey: "user-agent") ?? ""
+        })
         .padding()
     }
 }
